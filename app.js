a1e643acfb28b07d45f6381acd25b6faf682e228
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-01 16:43:19 -0500

    Random number is generating and showing up at /random

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ var express = require('express');
 
 var app = express();
 
+var port = 5000;
+
+app.use(express.static('public'));
+
+app.listen(port, function () {
+    console.log('the page is listening');
+});
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+var rando =randomNumber(0,10);
+app.get('/random', function(req, res){
+    res.send('Your random number is ' + rando);
+});
